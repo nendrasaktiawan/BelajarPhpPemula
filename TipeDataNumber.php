e072370d30c5ac740d5fe52e1f84e4034100ce8e
nendrasaktiawan <nendran433@gmail.com>
2024-07-10 13:16:36 +0700

    bagian3

---
+++ TipeDataNumber.php
@@ -0,0 +1,22 @@
+<?php 
+// integer
+echo "Decimal  : ";
+var_dump(1234);
+echo "Ocktal  : ";
+var_dump(01234);
+echo "Hexadecimal  : ";
+var_dump(0x1A);
+echo "Binary  : ";
+var_dump(0b111111);
+echo "Underscore in number  : ";
+var_dump(1_222_222);
+// floating point
+echo "Floating Point : ";
+var_dump(1.234);
+echo "Floating Point Dengan E notation Plus (1.7 x 1000) : ";
+var_dump(1.7e3);
+echo "Floating Point Dengan E notation Minus (1.7 x 0.001) : ";
+var_dump(1.7e-3);
+echo "Underscore di Floating Point: ";
+var_dump(1_367.773);
+?>
