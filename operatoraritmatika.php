7920e2abe8c5337e256989c68d6f7b98570076f1
nendrasaktiawan <nendran433@gmail.com>
2024-07-15 11:14:39 +0700

    bagian12

---
+++ operatoraritmatika.php
@@ -0,0 +1,22 @@
+<?php
+$result = 10 + 10;
+var_dump($result);
+$result = 100 % 3;
+var_dump($result); 
+
+// dalam bentuk variabel
+
+$a = 1;
+$b = 2;
+
+$variabel = $a + $b;
+var_dump($variabel);
+
+$variabelNegative = -$variabel;
+var_dump($variabelNegative);
+
+$variabelPositive = +$variabel;
+var_dump($variabelPositive);
+
+$variabelModulo = 200 % 7;
+var_dump($variabelModulo);
